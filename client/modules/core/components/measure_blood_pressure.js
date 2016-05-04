1f71cf7427d1fc04a5368443d09ce4ef92841394
floodpeak <hongfengzhou@yahoo.com>
2016-05-04 17:12:03 +0800

    start&stop discovery, cannot discover any thing

--- client/modules/core/components/measure_blood_pressure.js
+++ client/modules/core/components/measure_blood_pressure.js
@@ -3,6 +3,7 @@ import RaisedButton from 'material-ui/lib/raised-button'
 import IconButton from 'material-ui/lib/icon-button'
 import ActionHome from 'material-ui/lib/svg-icons/action/home'
 import AvPlayArrow from 'material-ui/lib/svg-icons/av/play-arrow'
+import AvStop from 'material-ui/lib/svg-icons/av/stop'
 
 class MeasureBloodPressure extends React.Component{
   render(){
@@ -13,13 +14,53 @@ class MeasureBloodPressure extends React.Component{
         </IconButton>
         <div>
           <p>We try BP Cordova Plugin here</p>
-          <RaisedButton label="Start"  icon={<AvPlayArrow />} onMouseDown={this.startMeasure}/>
+          <p><RaisedButton label="Start"  icon={<AvPlayArrow />} onMouseDown={this.startMeasure}/></p>
+          <p><RaisedButton label="Stop"  icon={<AvStop />} onMouseDown={this.stopMeasure}/></p>
         </div>
       </div>
     )
   }
   startMeasure(){
-    console.log("message");
+    const BP3LSecret="fa8fe120a6a2551755c720b8f6e805d5"
+    console.log(':)start Discovery')
+		BpManagerCordova.startDiscovery(function(res){
+      console.log("in callback");
+
+			//var device = BP3L.parseJSON(res)
+      console.log(res);
+
+			// if (device.address && device.name && device.name === "BP3L") {
+      //
+			// 	Meteor.clearTimeout(self.discoveryTimeoutTimer)
+      //
+      //
+			// 	//发现一个设备 停止发现
+			// 	BpManagerCordova.stopDiscovery((res)=>{
+			// 		console.log(res)
+			// 	}, (res)=>{
+			// 		console.log(res)
+			// 	}, BP3L.appsecret)
+      //
+      //
+			// 	callback(null, device)
+			// }
+
+		}, function(err){
+			// callback(err)
+			console.log(err);
+
+		}, BP3LSecret)
+
+
+  }
+  stopMeasure(){
+    const BP3LSecret="fa8fe120a6a2551755c720b8f6e805d5"
+    console.log(":)stop Discovery")
+    BpManagerCordova.stopDiscovery((res)=>{
+							console.log(res)
+						}, (err)=>{
+							console.log(err)
+						}, BP3LSecret)
 
   }
 }
---
+++ mobile-config.js
@@ -0,0 +1,24 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'com.ihealthlabs.bp.mantra.demo.android',
+  // id: 'com.ihealthlabs.bp.mantra.demo.ios',
+  name: 'iHealth',
+  description: 'iHealth BP Mantra Demo',
+  author: 'iHealthLabs',
+  website: 'http://www.ihealthlabs.com.cn',
+  version: '0.0.1',
+  buildNumber: '1'
+})
+
+App.accessRule('*');
+// App.accessRule("blob:*");
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#008CE0');
+App.setPreference('orientation', 'portrait');
+
+App.configurePlugin('com.ihealth.plugin.bp.bpmanagercordova', {
+  APP_ID: '2015042803',
+  API_KEY: 'supersecretapikey',
+  APP_NAME: 'BpManagerCordova'
+});
